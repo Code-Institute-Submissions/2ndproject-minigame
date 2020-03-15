e543adca901216333d8fe8e18d6753c19e568b88
TeoYangBin <ybteo88@gmail.com>
2020-03-15 03:53:22 +0000

    update random images path

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -72,16 +72,16 @@ function playFunction(){
 	var gametotal;
 	document.getElementById("handrandom1").innerHTML = random1;
 	if (random1 == 0){
-		document.getElementById("handrandom1").style.backgroundImage = url('../2ndproject-minigame/assets/images/fist.png');
+		document.getElementById("handrandom1").style.backgroundImage = "url('../2ndproject-minigame/assets/images/fist.png')";
 	}	else {
-		document.getElementById("handrandom1").style.backgroundImage = url('../2ndproject-minigame/assets/images/hand.png');
+		document.getElementById("handrandom1").style.backgroundImage = "url('../2ndproject-minigame/assets/images/hand.png')";
     }
 
 	document.getElementById("handrandom2").innerHTML = random2;
 	if (random2 == 0){
-		document.getElementById("handrandom2").style.backgroundImage = url('../2ndproject-minigame/assets/images/fist.png');
+		document.getElementById("handrandom2").style.backgroundImage = "url('../2ndproject-minigame/assets/images/fist.png')";
 	}	else {
-		document.getElementById("handrandom2").style.backgroundImage = url('../2ndproject-minigame/assets/images/hand.png');
+		document.getElementById("handrandom2").style.backgroundImage = "url('../2ndproject-minigame/assets/images/hand.png')";
 	}
 	
 //add up player total hand
